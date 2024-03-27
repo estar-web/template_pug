3e73d7b737a3030a9f39f79691be6b3c2f95ec53
ほし <cgastrogation@gmail.com>
2024-03-27 09:43:11 +0900

    [update]_もらったテンプレートに更新

--- gulpfile.js
+++ gulpfile.js
@@ -1,72 +1,193 @@
-const gulp = require("gulp");
-const sass = require("gulp-sass")(require("sass"));
-const postcss = require("gulp-postcss");
-const autoprefixer = require("autoprefixer");
-const cssSorter = require("css-declaration-sorter");
-const mmq = require("gulp-merge-media-queries");
-const htmlBeautify = require("gulp-html-beautify");
-const browserSync = require("browser-sync");
+const gulp = require('gulp');
+const del = require('del');
 
-/**
- * 変数設定
- */
-const path = {
-    root: 'src/',
-    // data: 'src/_data/site.json',
-    css: '/css/',
+//scss
+const sass = require('gulp-dart-sass'); //DartSassを使用
+const plumber = require("gulp-plumber"); // エラーが発生しても強制終了させない
+const notify = require("gulp-notify"); // エラー発生時のアラート出力
+const browserSync = require("browser-sync"); //ブラウザリロード
+const autoprefixer = require('gulp-autoprefixer'); //ベンダープレフィックス自動付与
+const postcss = require("gulp-postcss"); //css-mqpackerを使用
+const mqpacker = require('css-mqpacker'); //メディアクエリをまとめる
+
+//pugを使用
+const pug = require('gulp-pug');
+
+// 画像圧縮
+const change = require('gulp-changed');
+const imageMin = require("gulp-imagemin");
+const mozJpeg = require("imagemin-mozjpeg");
+const pngQuant = require("imagemin-pngquant");
+
+// webp変換
+const webp = require('gulp-webp'); //gulp-webpでwebp変換
+
+// 入出力するフォルダを指定
+const srcBase = './src';
+const srcBaseAssets = './src/assets';
+const distBase = './dist';
+const distBaseAssets = './dist/assets';
+
+
+const srcPath = {
+  'scss': srcBase + '/scss/**/*.scss',
+  'html': srcBase + '/**/*.html',
+  'img': srcBaseAssets + '/images/**/*',
+  'js': srcBaseAssets + '/js/*.js',
+  'pug': srcBase + '/pug/**/*.pug',
 };
 
-const src ={
-    html: '*.html',
-    sass: '/src/assets/sass/**/*.scss',
-    php: '*.php',
-    js: 'js/*.js',
+const distPath = {
+  'css': distBaseAssets + '/css/',
+  'html': distBase + '/',
+  'img': distBaseAssets + '/images/',
+  'js': distBaseAssets + '/js/'
+};
+
+
+/**
+ * clean
+ */
+const clean = () => {
+  return del([distBase + '/**'], {
+    force: true
+  });
 }
 
-const url ={
-    //PHPを使う時にここを有効にしてローカルアドレス入力
-    // local: "",
+//ベンダープレフィックスを付与する条件
+const TARGET_BROWSERS = [
+  'last 2 versions', //各ブラウザの2世代前までのバージョンを担保
+  'ie >= 11' //IE11を担保
+];
+
+
+/**
+ * pug
+ */
+const compilePug = () => {
+  // _*.pugはコンパイルしない
+  return gulp.src([srcPath.pug, '!./src/pug/**/_*.pug'])
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(
+      //エラーが出ても処理を止めない
+      plumber({
+        errorHandler: notify.onError('Error:<%= error.message %>')
+      }))
+    .pipe(gulp.dest(distPath.html))
+    .pipe(browserSync.stream())
+    .pipe(notify({
+      message: 'Pugをコンパイルしました！',
+      onLast: true
+    }))
+};
+
+
+/**
+ * sass
+ *
+ */
+const cssSass = () => {
+  return gulp.src(srcPath.scss, {
+      sourcemaps: true
+    })
+    .pipe(
+      //エラーが出ても処理を止めない
+      plumber({
+        errorHandler: notify.onError('Error:<%= error.message %>')
+      }))
+    .pipe(sass({
+      outputStyle: 'expanded'
+    })) //指定できるキー expanded compressed
+    .pipe(autoprefixer(TARGET_BROWSERS))
+    .pipe(postcss([mqpacker()])) // メディアクエリをまとめる
+    .pipe(gulp.dest(distPath.css, {
+      sourcemaps: './'
+    })) //コンパイル先
+    .pipe(browserSync.stream())
+    .pipe(notify({
+      message: 'Sassをコンパイルしました！',
+      onLast: true
+    }))
 }
 
 
-function compileSass() {
-    return gulp.src(src.sass)
-        .pipe(sass())
-        .pipe(postcss([autoprefixer(), cssSorter()]))
-        .pipe(mmq())
-        .pipe(gulp.dest(path.css))
+/**
+ * js
+ */
+const js = () => {
+  return gulp.src(srcPath.js)
+    .pipe(gulp.dest(distPath.js))
 }
 
-function watch() {
-    gulp.watch(src.sass, gulp.series(compileSass, browserReload));
-    gulp.watch(src.html, formatHTML);
-    gulp.watch(src.php, browserReload);
-    gulp.watch(src.js, browserReload);
+
+/**
+ * image
+ */
+const image = () => {
+  return gulp.src(srcPath.img)
+    .pipe(change(distPath.img))
+    .pipe(
+      imageMin([
+        pngQuant({
+          quality: [0.75, 0.8],
+          speed: 1,
+        }),
+        mozJpeg({
+          quality: 80,
+        }),
+        imageMin.svgo(),
+        imageMin.optipng(),
+        imageMin.gifsicle({ optimizationLevel: 3 })
+      ])
+    )
+    .pipe(webp())
+    .pipe(gulp.dest(distPath.img))
 }
 
-function formatHTML() {
-    return gulp.src(src.html)
-        .pipe(htmlBeautify({
-            indent_size: 2,
-            indent_with_tabs: true,
-        }))
-        // .pipe(gulp.dest("/"))
+
+/**
+ * ローカルサーバー立ち上げ
+ */
+const browserSyncFunc = () => {
+  browserSync.init(browserSyncOption);
 }
 
-function browserInit(done) {
-    //PHPファイルを扱う時にここの処理を有効にする
-
-    // browserSync.init({
-    //     proxy: url.local,                  //各々のローカルホストアドレスを記載
-    //     notify: false,                  // ブラウザ更新時に出てくる通知を非表示にする
-    //     open: "external",                // ローカルIPアドレスでサーバを立ち上げる
-    // });
-    done();
+const browserSyncOption = {
+  server: distBase
 }
 
-function browserReload(done) {
-    browserSync.reload();
-    done();
+/**
+ * リロード
+ */
+const browserSyncReload = (done) => {
+  browserSync.reload();
+  done();
 }
 
-exports.default = gulp.parallel(formatHTML, watch, compileSass, browserInit);
+
+/**
+ *
+ * ファイル監視 ファイルの変更を検知したら、browserSyncReloadでreloadメソッドを呼び出す
+ * series 順番に実行
+ * watch('監視するファイル',処理)
+ */
+const watchFiles = () => {
+  gulp.watch(srcPath.pug, gulp.series(compilePug, browserSyncReload))
+  gulp.watch(srcPath.scss, gulp.series(cssSass))
+  gulp.watch(srcPath.js, gulp.series(js, browserSyncReload))
+  gulp.watch(srcPath.img, gulp.series(image, browserSyncReload))
+}
+
+/**
+ * seriesは「順番」に実行
+ * parallelは並列で実行
+ *
+ * 一度cleanでdistフォルダ内を削除し、最新のものをdistする
+ */
+exports.default = gulp.series(
+  clean,
+  gulp.parallel(compilePug, cssSass, js, image),
+  gulp.parallel(watchFiles, browserSyncFunc)
+);
